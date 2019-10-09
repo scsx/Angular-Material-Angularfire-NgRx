9513d3ee98af37c1256fd6c87125d576b4dec72f
scsx <jsoucasaux@gmail.com>
2019-10-09 01:24:29 +0100

    End section 6

--- fitness-app/src/app/auth/auth.service.ts
+++ fitness-app/src/app/auth/auth.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { Router } from '@angular/router';
-import { IUser } from './user.model';
 import { IAuthData } from './auth-data.model';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { TrainingService } from '../training/training.service';
@@ -20,48 +19,54 @@ export class AuthService {
         private afAuth: AngularFireAuth,
         private trService: TrainingService) {}
 
+    initAuthListener() {
+        this.afAuth.authState.subscribe(user => {
+            if (user) {
+                this.isAuthenticated = true;
+                this.authChange.next(true);
+                this.router.navigate(['/training']);
+            } else {
+                this.trService.cancelSubscriptions(); // manage logout problems, aula 96
+                this.authChange.next(false);
+                this.router.navigate(['/login']);
+                this.isAuthenticated = false;
+            }
+        });
+    }
+
     registerUser(authData: IAuthData) {
         this.afAuth.auth.createUserWithEmailAndPassword(
             authData.email,
             authData.password
         ).then(result => {
-            this.authSuccessfully(true, '/training');
+            console.log(result);
         }).catch(error => {
             console.log(error);
         });
     }
 
     login(authData: IAuthData) {
-
         this.afAuth.auth.signInWithEmailAndPassword(
             authData.email,
             authData.password
         ).then(result => {
-            this.authSuccessfully(true, '/training');
+            console.log(result);
         }).catch(error => {
             console.log(error);
         });
     }
 
     logout() {
-        this.trService.cancelSubscriptions(); // manage logout problems, aula 96
-        this.isAuthenticated = false;
-        this.authSuccessfully(false, '/login');
+        this.afAuth.auth.signOut();
     }
 
     // getUser() because user: IUser is private
     /* getUser() {
-        return { ...this.user}; // spread operator to copy User without chaning the original
+        return { ...this.user}; // spread operator to copy User without changing the original
     } */
 
     isAuth() {
         return this.isAuthenticated;
-    }
-
-    private authSuccessfully(next: boolean, whereTo: string) {
-        this.isAuthenticated = true;
-        this.authChange.next(next);
-        this.router.navigate([whereTo]);
     }
 
 }
